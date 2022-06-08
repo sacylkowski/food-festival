6844808c2be597af5558aa1d5d0dcfe4b3618af1
sacylkowski <sacylkowski@gmail.com>
2022-06-08 14:45:17 -0500

    code-splitting for js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 // a blueprint file for the way webpack should behave within our project
-
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
 
 // the main configuration object - add options within this object that tell webpack what to do
 
 module.exports = {
-    entry: "./assets/js/script.js",
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        tickets: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js"
+    },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.bundle.js"
+        // filename is using the key from the entry points
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js"
     },
     // need to tell webpack to use the jQuery plugin
     plugins: [
@@ -17,6 +23,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",  // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: "development"
 };
